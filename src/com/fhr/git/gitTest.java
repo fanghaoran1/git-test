17c21c1957843c4c780ef775292f6f3c0e7321ce
fhr <fhr@2023learn.com>
2023-03-11 12:40:10 +0800

    commit2

--- src/com/fhr/git/gitTest.java
+++ src/com/fhr/git/gitTest.java
@@ -2,6 +2,8 @@ package com.fhr.git;
 
 public class gitTest {
     public static void main(String[] args) {
-
+        System.out.println("gitTest1");
+        System.out.println("gitTest2");
+        System.out.println("gitTest3");
     }
 }
